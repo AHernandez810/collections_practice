1271c8e21938bc6431cb7ef4e3748a9b4ab27eaa
ashley <ashleyhernandez810.ah@gmail.com>
2020-10-30 01:08:20 -0400

    commit tree 14cc2249babd48d45490ef19a2d09afaeb163a17

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(integer)
+    integer.sort do |a, b|
+        a <=> b
+    end 
+end 
 
+def sort_array_desc(integer)
+    integer.sort do |a, b|
+        b <=> a 
+    end 
+end
+
+def sort_array_char_count(string)
+    string.sort do |a, b|
+        a.length <=> b.length
+    end 
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end 
+
+def reverse_array(integer)
+    integer.reverse
+end
+
+def kesha_maker(string)
+    string.each do |kesha|
+        kesha[2] = "$"
+    end 
+end 
+
+def find_a(string)
+    string.find_all do |letter|
+        letter.chr == "a"
+    end 
+end
+
+def sum_array(integer)
+    integer.inject do|sum, num|
+        sum += num
+    end 
+end
+
+def add_s(array)
+    array.collect do |letter|
+        if array[1] == letter
+            letter
+        else
+            letter += "s"
+        end
+    end
+end 
